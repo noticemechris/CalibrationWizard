68a4df4fb6f5c1274e1884db1ce22d3132f68941
Adam Goodman <59903108+noticemeaikas@users.noreply.github.com>
2020-04-18 00:12:25 -0700

    This is geoquiz, with a few mods.

---
+++ app/README.md
@@ -0,0 +1,10 @@
+[Jared tells me to start with "Hi David"]
+Hi David. Welcome to the official readme for Adam Goodman's Etch-A-Sketch Laser Cutter calibration app. Although I could make this a long description of my app and senior project, Jared tells me there is a difference between what David COULD read and what David WANTS TO read. Without more to do, let's get into it.
+
+The objectives of this app are as follows:
+
+1. Greet the user when they open the app, and prompt them to connect via bluetooth to the Raspberry Pi controlling the laser cutter. I will use the following code for setting this up with an intent: http://blog.davidvassallo.me/2014/05/11/android-linux-raspberry-pi-bluetooth-communication/
+2. Prompt the user to place their preferred medium of engraving onto the 3D printer bed, and secure it. Once this is complete, the app will send a command to the laser cutter to "home" itself and use sensors to find out how far to place the laser above the engraving medium. This is a long process, but it will written onto the Raspberry Pi so I don't feel obligated to explain it here.
+3. Prompt the user to set the locations of each corner of the medium. I have an idea for how this will work, you'll kind of see it in the skeleton code.
+4. Once calibrated, the app will allow the user to turn or adjust the intensity of the laser on a scale of 1-255. I forget the name for this is called instrumentation-wise but you get the idea. It will also allow the user to re-home and re-calibrate the cutter if the cutting medium is shifted or replaced, and toggle between being "on" and the lowest possible setting (so they can see where the laser is pointing but not do anything else) if the user wants to move the head without engraving anything.
+5. I find it imperative that this app is ONLY in light theme. This is because the user will be wearing laser safety glasses (basically sunglasses) whilst operating it, and catastrophe may strike if it's a dark background with skinny, white text.
---
+++ app/src/main/java/org/overlake/mat803/calibrationwizard/QuizActivity.java
@@ -0,0 +1,175 @@
+package org.overlake.mat803.calibrationwizard;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.view.Gravity.BOTTOM;
+import static android.view.Gravity.CENTER_HORIZONTAL;
+import static android.view.Gravity.CENTER_VERTICAL;
+
+public class QuizActivity extends AppCompatActivity {
+
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private Button mCheatButton;
+
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank;
+    private int mCurrentIndex;
+    public static final String TAG = "QuizActivity";
+    public static final String KEY_INDEX = "currentIndex";
+    private static final int REQUEST_CODE_CHEAT = 0;
+
+    private boolean mIsCheater;
+
+    public QuizActivity(){
+        mQuestionBank = new Question[]{
+                new Question(R.string.question_australia, true),
+                new Question(R.string.question_oceans, true),
+                new Question(R.string.question_mideast, false),
+                new Question(R.string.question_africa, false),
+                new Question(R.string.question_americas, true),
+                new Question(R.string.question_chris, true),
+                new Question(R.string.question_asia, true)
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "onCreate() called");
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
+        updateQuestion();
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mCheatButton = findViewById(R.id.cheat_button);
+
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_CHEAT && data != null){
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+    private void updateQuestion() {
+        mIsCheater = false;
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+
+        int messageResId = 0;
+        if(mIsCheater){
+            messageResId = R.string.judgment_toast;
+        } else {
+
+            boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+
+        }
+
+        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.TOP, CENTER_HORIZONTAL, CENTER_VERTICAL);
+        t.show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState() called()");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+}
